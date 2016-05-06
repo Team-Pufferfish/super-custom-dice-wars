c56dae357dee356b8bacb9427ba4ffa51507d0e7
Graphic <graphic@SOD-SJH-503-13M.local>
2016-05-06 15:35:24 -0400

    base game graphics disconnected from state

--- src/Index.js
+++ src/Index.js
@@ -11,22 +11,87 @@ let store = createStore(appstate,0,
 
 var style = { font: "65px Arial", fill: "#ff0044", align: "center" };
 
-var game = new Phaser.Game(800, 600, Phaser.Canvas, 'cube-party', { create });
+var screenX = 1024;
+var screenY = 768;
+var game = new Phaser.Game(screenX, screenY, Phaser.Canvas, 'cube-party', { preload, create, render });
 
 
 function setText(text) {
   text.setText("- You have clicked -\n" + store.getState().appstate + " times !");
 }
 
+function preload(){
+  //Background
+  game.load.image('background', 'dist/images/wood.jpg');
+  game.load.image('cups','dist/images/cups.png');
+  //TilePieces
+  game.load.image('tileRed','dist/images/tileRed.png');
+  game.load.image('tileBlue','dist/images/tileBlue.png');
+  game.load.image('tileRedStart','dist/images/tileRedStart.png');
+  game.load.image('tileBlueStart','dist/images/tileBlueStart.png');
+  game.load.image('tileNeutral','dist/images/tileNeutral.png');
+  //dice
+  game.load.spritesheet('redDice',"dist/images/DieRed.png",85,85,6);
+  //targets
+  game.load.image('redTargets','dist/images/tileRedMove.png');
+  game.load.image('blueTargets','dist/images/tileRedBlue.png');
+}
+
+var background;
+var cupRed,cupBlue;
+var cupHeight = 200;
+var cupWidth = 450;
+var board;
+var boardHeight = 4;
+var boardWidth = 6;
+var tileDim = 127;
+
+//groups
+var tiles;
+var redTargets;
+var blueTargets;
+
 function create() {
-  let text = game.add.text(game.world.centerX,game.world.centerY, "hello",style);
-  text.anchor.set(0.5);
-  text.inputEnabled = true;
-  text.input.enableDrag();
+  background = game.add.image(0,0,'background');
+  cupRed = game.add.image(0,screenY-cupHeight,'cups');
+  cupBlue = game.add.image(screenX - cupWidth, screenY-cupHeight,"cups")
 
-  store.subscribe(setText.bind(null,text));
+  //groups
+  tiles = game.add.group();
+  redTargets = game.add.group();
+  blueTargets = game.add.group();
 
-  text.events.onInputDown.add(() => {
-    store.dispatch(add())
-  });
+  //draw tiles
+  var col,row;
+  for(col = 0; col < boardWidth; col++){
+    for(row = 0; row < boardHeight; row++){
+      var spriteimage = "tileNeutral";
+      var spriteX = screenX/2 - ((boardWidth)/2 * tileDim) + (col * tileDim);
+      var spriteY = screenY/2 - ((boardHeight/2) * tileDim) + (row * tileDim) -100;
+
+      if (col === 0)
+        spriteimage = "tileRedStart";
+      else if (col === boardWidth - 1)
+        spriteimage = "tileBlueStart";
+      else if (col < boardWidth/2)
+        spriteimage = "tileRed";
+      else if (col >= boardWidth/2)
+        spriteimage = "tileBlue";
+      //console.log("X:" + spriteX + " Y:" +spriteY+ " S:" + spriteimage);
+      tiles.create(spriteX,spriteY,spriteimage);
+      if (col !== boardWidth-1)
+        redTargets.create(spriteX,spriteY,"redTargets");
+      if(col !== 0)
+        blueTargets.create(spriteX,spriteY,"blueTargets");
+    }
+  }
+
+  redTargets.alpha = 0.0;
+  blueTargets.alpha = 0.0;
+
+
+}
+
+function render() {
+
 }
