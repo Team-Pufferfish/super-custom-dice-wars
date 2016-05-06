dbb69549abc8c8e0722e0f2008b1a7b7912ac3c1
Graphic <graphic@SOD-SJH-503-12M.local>
2016-05-06 11:36:31 -0400

    initial commit

---
+++ src/Index.js
@@ -0,0 +1,31 @@
+import { add, remove } from './actions';
+import { createStore, compose } from 'redux';
+import  appstate  from './reducers';
+
+global.PIXI = require('../node_modules/phaser-shim/dist/pixi');
+global.Phaser = require('../node_modules/phaser-shim/dist/phaser');
+
+let store = createStore(appstate,0,
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+
+var style = { font: "65px Arial", fill: "#ff0044", align: "center" };
+
+var game = new Phaser.Game(800, 600, Phaser.Canvas, 'cube-party', { create });
+
+
+function setText(text) {
+  text.setText("- You have clicked -\n" + store.getState() + " times !");
+}
+
+function create() {
+  let text = game.add.text(game.world.centerX,game.world.centerY, "hello",style);
+  text.anchor.set(0.5);
+  text.inputEnabled = true;
+
+  store.subscribe(setText.bind(null,text));
+
+  text.events.onInputDown.add(() => {
+    store.dispatch(add())
+  });
+}
